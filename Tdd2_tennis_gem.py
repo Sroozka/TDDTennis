44b7da9307ee1df28f4ee1ec7caa098b3f1a71cb
Zimny <marcin.f.zimny@gmail.com>
2020-05-06 10:34:01 +0200

    uncommented gem_result()

--- Tdd2_tennis_gem.py
+++ Tdd2_tennis_gem.py
@@ -7,8 +7,8 @@ class TennisGem:
         self.Who_is_leading = " "
         self.Gem_ended = False
 
-    # def gem_result(self):
-    #     return f"{TennisGem.listOfNames[self.P1_ball_score]}-{TennisGem.listOfNames[self.P2_ball_score]}"
+    def gem_result(self):
+        return f"{TennisGem.listOfNames[self.P1_ball_score]}-{TennisGem.listOfNames[self.P2_ball_score]}"
 
     def p1_scored_ball(self):
         if 4 == self.P2_ball_score and self.P1_ball_score == 3:
