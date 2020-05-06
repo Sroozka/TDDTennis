218c026bb920b9b6ed26d064c36c99b8379e8585
Zimny <marcin.f.zimny@gmail.com>
2020-05-06 10:12:07 +0200

    added init(), begin_gem(), is_gem_running(), add_points

--- TDD2_tennis_set.py
+++ TDD2_tennis_set.py
@@ -1,7 +1,22 @@
 from Tdd2_tennis_gem import TennisGem
 
+
 class TennisSet:
     def __init__(self):
-        list_of_gems = []
-        set_winner = " "
-        set_ended = False
+        self.gem = TennisGem()
+        self.list_of_gems = []
+        self.set_winner = " "
+        self.set_ended = False
+
+    def begin_gem(self):
+        if self.list_of_gems == [] or self.is_gem_running():  # is True
+            pass
+
+    def is_gem_running(self):
+        if self.list_of_gems[-1]:  # is True
+            return False
+        else:
+            return True
+
+    def add_points(self, p1_score, p2_score):
+        pass
