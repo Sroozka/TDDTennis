995423a14debfd27a65656691823229b72caf91b
Zimny <marcin.f.zimny@gmail.com>
2020-05-06 10:30:24 +0200

    added win_gem()

--- TDD2_tennis_set.py
+++ TDD2_tennis_set.py
@@ -5,6 +5,7 @@ class TennisSet:
     def __init__(self):
         self.gem = TennisGem()
         self.list_of_gems = []
+        self.list_of_gem_scores = []
         self.set_winner = " "
         self.set_ended = False
 
@@ -18,8 +19,20 @@ class TennisSet:
         else:
             return True
 
-    def add_points(self, p1_score, p2_score):
-        pass
+    def win_gem(self, p1_score, p2_score):
+        if p1_score <= p2_score:  # 2:4
+            if p1_score != 0:  # x2
+                self.gem_P1_P2_scored(p1_score)
+            for _ in range(p1_score, p2_score):  # range(2,4) = 2
+                self.gem.p2_scored_ball()
+            self.list_of_gems.append(self.gem.Gem_ended)
+
+        else:  # 5:3
+            if p2_score != 0:  # x3
+                self.gem_P1_P2_scored(p2_score)
+            for _ in range(p2_score, p1_score):  # range(3,5) = 2
+                self.gem.p1_scored_ball()
+            self.list_of_gems.append(self.gem.Gem_ended)
 
     def gem_P1_P2_scored(self, punkty):
         for _ in range(punkty):
