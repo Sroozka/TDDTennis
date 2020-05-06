426f98872844ae48683359a0ea736c005727f182
Zimny <marcin.f.zimny@gmail.com>
2020-05-07 00:27:58 +0200

    changed gem class, added scores to init

--- Tdd2_tennis_gem.py
+++ Tdd2_tennis_gem.py
@@ -1,14 +1,34 @@
 class TennisGem:
     listOfNames = ("0", "15", "30", "40", "Advantage", "Gem")
 
-    def __init__(self):
+    def __init__(self, p1_scored, p2_scored):
         self.P1_ball_score = 0
         self.P2_ball_score = 0
+        self.p1_scored = p1_scored
+        self.p2_scored = p2_scored
         self.Who_is_leading = " "
         self.Gem_ended = False
 
     def gem_result(self):
         return f"{TennisGem.listOfNames[self.P1_ball_score]}-{TennisGem.listOfNames[self.P2_ball_score]}"
+
+    def win_gem(self):
+        if self.p1_scored <= self.p2_scored:  # 2:4
+            if self.p1_scored != 0:  # x2
+                self.gem_P1_P2_scored(self.p1_scored)
+            for _ in range(self.p1_scored, self.p2_scored):  # range(2,4) = 2
+                self.p2_scored_ball()
+        else:  # 5:3
+            if self.p2_scored != 0:  # x3
+                self.gem_P1_P2_scored(self.p2_scored)
+            for _ in range(self.p2_scored, self.p1_scored):  # range(3,5) = 2
+                self.p1_scored_ball()
+
+    def gem_P1_P2_scored(self, punkty):
+        for _ in range(punkty):
+            self.p1_scored_ball()
+            self.p2_scored_ball()
+
 
     def p1_scored_ball(self):
         if 4 == self.P2_ball_score and self.P1_ball_score == 3:
