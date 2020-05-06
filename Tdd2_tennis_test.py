b3f688dbff342f4323514050b2e76ddaedc197f1
Zimny <marcin.f.zimny@gmail.com>
2020-05-07 00:29:28 +0200

    changed tests to scoring in gem class

--- Tdd2_tennis_test.py
+++ Tdd2_tennis_test.py
@@ -4,26 +4,25 @@ from Tdd2_tennis_set import TennisSet
 
 
 class TennisTest2(unittest.TestCase):
-    def setUp(self):
-        self.gem = TennisGem()
-        self.set = TennisSet()
-        pass
+    # def setUp(self):
+    #     self.gem = TennisGem()
+    #     self.set = TennisSet()
+    #     pass
 
     def test1_start(self):
-        initial_gem_score = self.gem.gem_result()
-        self.assertEqual("0-0", initial_gem_score)
+        self.gem = TennisGem(0, 0)
+        self.gem.win_gem()
+        self.assertEqual("0-0", self.gem.gem_result())
 
     def test2_P1_15_0(self):
-        self.gem.p1_scored_ball()
-        gem_score = self.gem.gem_result()
-        self.assertEqual("15-0", gem_score)
+        self.gem = TennisGem(1, 0)
+        self.gem.win_gem()
+        self.assertEqual("15-0", self.gem.gem_result())
 
     def test2_P2_15_30(self):
-        self.gem.p1_scored_ball()
-        self.gem.p2_scored_ball()
-        self.gem.p2_scored_ball()
-        gem_score = self.gem.gem_result()
-        self.assertEqual("15-30", gem_score)
+        self.gem = TennisGem(1, 2)
+        self.gem.win_gem()
+        self.assertEqual("15-30", self.gem.gem_result())
 
     # def test2_P1_P2_15_40_set(self):
     #     self.set.win_gem(1, 3)
